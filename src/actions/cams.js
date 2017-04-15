710d1451c9b2f9c70ccf852780c93ad70576b77c
Thomas Beaufoy <beaufoyyy@live.com>
2017-04-15 16:31:30 +0100

    Sorts out npm shrinkwrap and adds random request time

--- src/actions/cams.js
+++ src/actions/cams.js
@@ -8,14 +8,21 @@ const receiveCams = (cams, category) => ({ type: types.RECEIVE_CAMS, cams, categ
 const requestCams = () => ({ type: types.REQUEST_CAMS });
 export const filterRegion = (id) => ({ type: types.FILTER_REGION, id });
 
+function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 export function fetchCams(category) {
   const fetch = (dispatch) => {
     dispatch(requestCams());
 
+    const requestTime = getRandomArbitrary(0, 3) * 1000;
+    console.log(requestTime);
+
     const list = fromJS(camData);
     setTimeout(() => {
       dispatch(receiveCams(list.getIn(['camList']), category));
-    }, 2000);
+    }, requestTime);
   };
 
   return fetch;
