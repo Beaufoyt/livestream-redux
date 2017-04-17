f92c78e245dfd12960bf8e3759a04008ce40cf84
Thomas Beaufoy <beaufoyyy@live.com>
2017-04-17 21:53:58 +0100

    Card images now come from data

--- src/components/CamGrid.js
+++ src/components/CamGrid.js
@@ -14,14 +14,16 @@ export default class CamGrid extends PureComponent {
       const region = cam.get('region');
 
       if (currentFilter === null || region === currentFilter) {
-        return (
+        const card = (
           <li key={ index } className="user-item">
-            <div id="card-image" className="card-image" />
+            <div id="card-image" style={{ backgroundImage: `url(${cam.get('image')})` }} className="card-image" />
             <span className="pull-left user">{ cam.getIn(['name']) }</span>
             &nbsp;<span>{ cam.getIn(['region']) }</span>
             <span className="pull-right gender">{ cam.getIn(['category']) }</span>
           </li>
         );
+
+        return card;
       }
 
       return null;
